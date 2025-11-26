3f8a5004fc5e3df48972b84f134254a6c979190a
JagpreetJakhar <jagpreetjakhar94@gmail.com>
2025-11-26 12:17:22 +0000

    EDA blog

--- data/portfolio.tsx
+++ data/portfolio.tsx
@@ -50,37 +50,40 @@ const experience = {
   title: "Experience",
   experiences: [
     {
-      title: "Graduate Teaching Assistant- Big Data Analytics",
-      company: "University of Sheffield",
-      duration: "Jan 2024-Aug 2024",
-      description:
-        "Provided hands-on support for students working with AWS SageMaker, Athena, and Databricks to perform data \
- analytics queries and implement data mining algorithms at scale. ",
+      title: "Founding Engineer - Machine Learning",
+      company: "Idea Enterprises LLC",
+      duration: "Feb 2025 - Present",
+      description: `- Took Product from prototype stage to production.
+- Curated Custom Dataset combining Open Source Datasets, Sources and proprietary data
+- Finetuned Pre-trained Object Detection and Action recognition Models on Custom Dataset using Distributed Data Parallel Training Approach with high Top-1(87%) Top-5 Accuracy(95%), and F-1 Score(91%)
+- Built End-to-End low latency (<1s) inference pipeline for real-time system with high throughput (25 CCTVs per node) for Weapon detection and Violence from live CCTV streams.
+- Deployed system using Nvidia Deepstream and Apache Kafka, integrating with Front-End Application providing Real-time Monitoring and Alerts.`
     },
     {
-      title: " Graduate Teaching Assistant- Database Design",
+      title: "Graduate Teaching Assistant",
       company: "University of Sheffield",
-      duration: " Jan 2024-Jul 2024",
-      description:
-        "Guided students through the process of designing and implementing relational databases,common challenges related \
- to large and heterogeneous datasets, such as transactional and geo-spatial data.",
-    },
-    {
-      title: "Graduate Teaching Assistant- Data Modelling and Storage",
-      company: "University of Sheffield",
-      duration: "Sep 2023-Jan 2024",
-      description:
-        "Supported teaching by preparing presentations on topics such as Database Normalisation, providing technical \
- assistance to students during sessions on SQLite and Python integration, and addressing any questions. ",
+      duration: "Sep 2023 - Aug 2024",
+      description: `Big Data Analytics - Sheffield, UK
+
+Supporting students in understanding modern scale-out system architectures for processing large, structured and unstructured datasets, and how to scale resources for data processing based on workload requirements.
+Provided hands-on support for students working with Airflow, Kafka, AWS, and Databricks to perform data analytics queries and implement data mining algorithms at scale.
+Exploratory Data Analysis and Hypothesis Testing for Data Analysis.
+
+Database Design
+Helping students understand the fundamentals of database management, relational database design, and data warehousing and making ETL pipelines.
+Guided students through the process of designing and implementing relational databases, common challenges related to large and heterogeneous datasets, such as transactional and geo-spatial data.
+
+Data Modelling and Storage
+Supported teaching by preparing presentations on topics such as Database Normalisation, providing technical assistance to students during sessions on SQLite and Python integration, and addressing any questions.`
     },
     {
       title: "Software Engineer",
       company: "Swig Solutions",
-      duration: "2015-2019",
-      description:
-        "Developed and deployed responsive, user-centric dynamic web applications using HTML, CSS, and JavaScript and \
- React ",
-    },
+      duration: "2015 - 2019",
+      description: `Spearheaded the development of a Kiosk Information Management System, increasing operational efficiency and reducing manual inquiries by 40%.
+Developed and deployed responsive, user-centric dynamic web applications using HTML, CSS, JavaScript, and React, enhancing client satisfaction and retention.
+Conducted comprehensive code reviews and implemented rigorous testing protocols, reducing post-deployment defects by 25%.`
+    }
   ],
 };
 
@@ -93,18 +96,14 @@ const skills = {
         "Python",
         "PySpark",
         "PyTorch",
-        "JavaScript",
-        "TypeScript",
-        "React.js",
-        "Next.js",
-        "Node.js",
+        "CUDA",
         "Flask",
-        "Django"
+        "FastAPI"
       ],
     },
     {
       title: "Others",
-      skills: ["mlflow", "OpenCV","AWS","Airflow","Kafka","Docker","Terraform","PostgreSQL", "SQLite"],
+      skills: ["Mlflow", "OpenCV","AWS","Airflow","Kafka","Docker","Deepstream","PostgreSQL", "SQLite"],
     },
   ],
 };
